e8e9af2d633f2e86d14cf0bb90ec9cc97ea35ef3
admin <admin@ifs.local>
2017-07-22 14:48:51 -0300

    Primeiro projeto IOS

---
+++ IOS/Que Bom/Que Bom/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Que Bom
+//
+//  Created by admin on 22/07/17.
+//  Copyright © 2017 IFPB. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
