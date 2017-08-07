227752ff95aa344405949037942654b82b950967
admin <admin@ifs.local>
2017-08-07 15:05:16 -0300

    Janela

---
+++ IOS/Janela/Janela/OLD.swift
@@ -0,0 +1,43 @@
+//
+//  Tela2ViewController.swift
+//  Janela
+//
+//  Created by admin on 07/08/17.
+//  Copyright © 2017 IFPB. All rights reserved.
+//
+
+import UIKit
+
+class OLD: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Tela 2: viewDidLoad()")
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("Tela2: viewWillAppear()")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        print("Tela2: viewDidAppear()")
+    }
+
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ IOS/Janela/Janela/Tela1ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Janela
+//
+//  Created by admin on 07/08/17.
+//  Copyright © 2017 IFPB. All rights reserved.
+//
+
+import UIKit
+
+class Tela1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Tela 1: viewDidLoad()")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
